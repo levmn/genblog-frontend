e05c6506127f1a2474846a1c261f8e9af219537e
levi <nanowebcontato@gmail.com>
2022-02-16 12:07:35 -0300

    primeiros componentes do blog

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home(){
+    return(
+        <> 
+            <h1 className="titulo">Home</h1>
+            <img src="https://imgur.com/HHHzWIS.png" alt="Imagem da tela inicial, mariposa cecrópia." className="img"/>
+        </>
+    );
+}
+
+export default Home;
